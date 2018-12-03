59220e5eaada26884ed9e7f81151eb49ec72f821
wlthomson <will.tho@outlook.com>
2018-12-03 16:02:00 +1300

    Add multiple stacked bar plots

--- victory-react/src/App.js
+++ victory-react/src/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryTheme,
+  VictoryStack
+} from 'victory';
 import './App.css';
 
-const data = [
+const data2012 = [
   { quarter: 1, earnings: 13000 },
   { quarter: 2, earnings: 16500 },
   { quarter: 3, earnings: 14250 },
   { quarter: 4, earnings: 19000 }
+];
+
+const data2013 = [
+  { quarter: 1, earnings: 15000 },
+  { quarter: 2, earnings: 12500 },
+  { quarter: 3, earnings: 19500 },
+  { quarter: 4, earnings: 13000 }
+];
+
+const data2014 = [
+  { quarter: 1, earnings: 11500 },
+  { quarter: 2, earnings: 13250 },
+  { quarter: 3, earnings: 20000 },
+  { quarter: 4, earnings: 15500 }
+];
+
+const data2015 = [
+  { quarter: 1, earnings: 18000 },
+  { quarter: 2, earnings: 13250 },
+  { quarter: 3, earnings: 15000 },
+  { quarter: 4, earnings: 12000 }
 ];
 
 class App extends Component {
@@ -18,7 +45,12 @@ class App extends Component {
           tickFormat={['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4']}
         />
         <VictoryAxis dependentAxis tickFormat={x => `$${x / 1000}k`} />
-        <VictoryBar data={data} x="quarter" y="earnings" />
+        <VictoryStack>
+          <VictoryBar data={data2012} x="quarter" y="earnings" />
+          <VictoryBar data={data2013} x="quarter" y="earnings" />
+          <VictoryBar data={data2014} x="quarter" y="earnings" />
+          <VictoryBar data={data2015} x="quarter" y="earnings" />
+        </VictoryStack>
       </VictoryChart>
     );
   }
