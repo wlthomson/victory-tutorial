e63c99db70c6c182e7a2f8597c5e82118c249378
wlthomson <will.tho@outlook.com>
2018-12-03 15:31:27 +1300

    Add basic bar plot

--- victory-react/src/App.js
+++ victory-react/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { VictoryBar } from 'victory';
 import './App.css';
+
+const data = [
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 }
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Victory Tutorial</h1>
-      </div>
+      <VictoryBar>
+        data={data}
+        x="quarter" y="earnings"
+      </VictoryBar>
     );
   }
 }
